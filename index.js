bedcfd35f177295dc9be6c594eb5eb59fb409c12
KevinGr <kgomezr2@unicartagena.edu.co>
2025-03-13 08:37:05 -0500

    update files

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 const loginBtn = document.getElementById("login");
 const form_pic = document.getElementById("form-userPic");
 const inputPost = document.getElementById("post");
+const containerPosts = document.getElementById("container-posts");
+const btn_Post = document.getElementById("post-btn");
+
+const fecha = new Date();
+let mes = fecha.getMonth()
+let dia = fecha.getDate()
+
+
+switch(mes){
+    case 0:
+        mes = "EN";
+        break;
+    case 1:
+        mes = "FEB"
+        break
+    case 2:
+        mes = "MAR"
+        break;
+    case 3:
+        mes = "ABR"
+        break
+    case 4: 
+        mes = "MAY"
+        break
+    case 5: 
+        mes = "JUN"
+        break
+    case 6: 
+        mes = "JUL"
+        break
+    case 7:
+        mes = "AGO"
+        break
+    case 8:
+        mes = "SEP"
+        break
+    case 9:
+        mes = "OCT"
+        break
+    case 10:
+        mes = "NOV";
+        break
+    case 11:
+        mes = "DIC"
+        break
+}
 
 const DB = []
 const usuario = {
@@ -49,6 +95,8 @@ function createPost(name,username){
     userPic.classList.add("user-pic");
     userPic.textContent = userLogin.nombre.charAt(0);
 
+    picContent.appendChild(userPic);
+
     const contentPost = document.createElement('div');
     contentPost.classList.add("content-post");
 
@@ -59,12 +107,45 @@ function createPost(name,username){
     userName.classList.add("username-info");
     userName.textContent = userLogin.nombre;
 
+    userInfo.appendChild(userName);
+
     const postInfo = document.createElement('span')
     postInfo.classList.add("post-info");
-    postInfo.textContent = `${userLogin.usuario} • MAR 12`;
+    postInfo.textContent = ` ${userLogin.usuario} • ${mes} ${dia}`;
+
+    userInfo.appendChild(postInfo);
 
     const pText = document.createElement('p');
     pText.textContent= inputPost.value;
+
+    contentPost.appendChild(postInfo);
+    contentPost.appendChild(pText);
+    section.appendChild(picContent);
+    section.appendChild(contentPost);
+    document.getElementById("container-posts").appendChild(section);
+    inputPost.value="";
 }
 
+function search(busqueda){
+    const sections = document.querySelectorAll('section');
+    const regex = new RegExp(busqueda, 'i'); // Expresión regular dinámica (insensible a mayúsculas/minúsculas)
+
+    sections.forEach(section => {
+        let nombre = section.querySelector('.username-info');
+
+        if (nombre && regex.test(nombre.textContent)) { // Verifica que nombre no sea null
+            section.style.display = "flex";
+        } else {
+            section.style.display = "none"; // Oculta si no coincide
+        }
+    });
+}
+const inputSearch = document.getElementById("search");
+inputSearch.addEventListener('input',()=>{
+    search(inputSearch.value)
+})
+
 loginBtn.addEventListener("click",login)
+btn_Post.addEventListener('click',()=>{
+    createPost(userLogin.nombre,userLogin.usuario)
+})
